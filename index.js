943da43411085de8e09ad39b088ae0d435ea4986
MadiniMoyoni <info@madinimoyoni.co.uk>
2025-06-19 20:41:00 +0100

    Updates

--- index.js
+++ index.js
@@ -147,8 +147,9 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true
+    outDir: path.resolve(import.meta.dirname, "dist"),
+    emptyOutDir: true,
+    base: "/"
   }
 });
 
